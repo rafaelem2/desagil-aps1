1fab0e83c1f494c282bffba2d94b40edef15ca03
rafaelem2 <rafaelem2@al.insper.edu.br>
2021-03-20 21:29:44 -0300

    classe pedido
    classe pedido feita

--- VaiMalte/src/br/edu/insper/desagil/vaimalte/Pedido.java
+++ VaiMalte/src/br/edu/insper/desagil/vaimalte/Pedido.java
@@ -1,7 +1,29 @@
 package br.edu.insper.desagil.vaimalte;
 
 public class Pedido {
-	private String produto;
+	private Produto produto;
 	private int quantidade;
+	
+	public Pedido(Produto produto) {
+		super();
+		this.produto = produto;
+		this.quantidade = 1;
+	}
+	
+	public void incrementaQuantidade() {
+		this.quantidade = this.quantidade + 1;
+	}
+	
+	public double precoPedido() {
+		return this.produto.getPreco() * this.quantidade;
+	}
 
+	public Produto getProduto() {
+		return this.produto;
+	}
+
+	public int getQuantidade() {
+		return this.quantidade;
+	}
+	
 }
