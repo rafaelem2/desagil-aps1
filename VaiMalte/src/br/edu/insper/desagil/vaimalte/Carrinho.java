e14ecbbb0ff4e5aa850f4990cbb6f86ebce15a39
rafaelem2 <rafaelem2@al.insper.edu.br>
2021-03-20 21:30:59 -0300

    Carrinho
    carrinho feito

--- VaiMalte/src/br/edu/insper/desagil/vaimalte/Carrinho.java
+++ VaiMalte/src/br/edu/insper/desagil/vaimalte/Carrinho.java
@@ -1,10 +1,32 @@
 package br.edu.insper.desagil.vaimalte;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Carrinho {
-	private List<String> pedidos;
-	
-	
+	private List<Pedido> Pedidos;
 
+	public Carrinho() {
+		super();
+		this.Pedidos = new ArrayList<>();
+		
+	}
+
+	public List<Pedido> getPedidos() { 
+		return this.Pedidos;
+	}
+	
+	public void adicionaProduto(Produto produto) {
+		boolean Produtonovo = true;
+		for (Pedido pedido: this.Pedidos) {
+			if (pedido.getProduto().equals(produto)) {
+				pedido.incrementaQuantidade();
+				Produtonovo = false;
+			}
+		}
+		if (Produtonovo) {
+			Pedido Pedidonovo = new Pedido(produto);
+			this.Pedidos.add(Pedidonovo);
+		}
+	}
 }
