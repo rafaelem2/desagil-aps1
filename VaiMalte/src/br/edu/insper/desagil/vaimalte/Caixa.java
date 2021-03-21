da010b5f4c1e3f434ff98ed9b9c21e5433f7b475
JoaoVitorMenezesMagalhaes <joaovmm@al.insper.edu.br>
2021-03-20 23:51:54 -0300

    Caixa

--- VaiMalte/src/br/edu/insper/desagil/vaimalte/Caixa.java
+++ VaiMalte/src/br/edu/insper/desagil/vaimalte/Caixa.java
@@ -1,5 +1,34 @@
 package br.edu.insper.desagil.vaimalte;
 
-public class Caixa {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Caixa {
+	private Map<Integer, Integer> descontos;
+
+	public Caixa() {
+		super();
+		this.descontos = new HashMap<>();
+	}
+	
+	public void ComDesconto(Produto produto, int desconto) {
+		if ((desconto >= 1) && (desconto <= 99)) {
+			this.descontos.put(produto.getCodigo(), desconto);
+		}
+	}
+	
+	public double CompraTotal(Carrinho carrinho) {
+		double valortotal = 0.0;
+		for (Pedido pedido: carrinho.getPedidos()) {
+			if (this.descontos.containsKey(pedido.getProduto().getCodigo())) {
+				double desconto = descontos.get(pedido.getProduto().getCodigo());
+				valortotal = valortotal + ((pedido.getProduto().getPreco() * pedido.getQuantidade()) * (desconto/100));
+			}
+			else {
+				valortotal = valortotal + pedido.precoPedido();
+			}
+		}
+		return valortotal;
+	}
+	
 }
